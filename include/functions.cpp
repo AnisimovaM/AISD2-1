9a99683e04dd7c9140129c91014f61f12ec221ef
AnisimovaM <146504054+AnisimovaM@users.noreply.github.com>
2024-05-02 22:34:13 +0400

    first

---
+++ include/functions.cpp
@@ -0,0 +1,109 @@
+#include <fstream>
+#include <iostream>
+
+
+using namespace std;
+
+namespace tree {
+
+	template<typename T>
+	struct Node {
+
+		T _value;
+		Node<T>* _left;
+		Node<T>* _right;
+		
+
+		Node(T number) : _value(number), _left(nullptr), _right(nullptr) {}
+		Node(T number, Node* ref1, Node* ref2) : _value(number), _left(ref1), _right(ref2){}
+
+	};
+
+	template<typename T>
+	class Set {
+	private:
+
+		Node<T>* _root;
+
+		Node<T>* copying(Node<T>* root) {
+			if (root) {
+				Node<int>* copied_el = new Node<int>(root->_value);
+				copied_el->_left = copying(root->_left);
+				copied_el->_right = copying(root->_right);
+				return copied_el;
+			}
+			return nullptr;
+		}
+
+		bool adding_element(Node<T>*& root, T val) {
+			if (!root) {
+				root = new Node(val);
+				return true;
+			}
+			if (val < root->_value) return adding_element(root->_left, val);
+			else if (val > root->_value) return adding_element(root->_right, val);
+			return false;
+		}
+
+		bool element_presence(Node<T>*& root, T val) {
+			if (!root) {
+				return false;
+			}
+			if (val < root->_value) return element_presence(root->_left, val);
+			else if (val > root->_value) return element_presence(root->_right, val);
+			return true;
+		}
+
+		bool delete_element(Node<T>*& root, T val) {
+			if (!root) {
+				return false;
+			}
+			if (val < root->_value) return delete_element(root->_left, val);
+			else if (val > root->_value) return delete_element(root->_right, val);
+			else {
+				if (!root->_left) {
+					Node<T>* node = root->_right;
+					delete root;
+					root = node;
+				}
+				else if (!root->_right) {
+					Node<T>* node = root->_left;
+					delete root;
+					root = node;
+				}
+				else if (!root->_right && !root->_left) {
+					delete root;
+				}
+				else {
+					Node<T>* node = root->_left;
+					while (node->_right) {
+						node = node->_right;
+					}
+					root->_value = node->_value;
+					delete_element(root->_left, node->_value);
+				}
+				return true;
+			}
+		}
+
+		void deletion(Node<T>* root) {
+			if (root) {
+				deletion(root->_left);
+				deletion(root->_right);
+				delete root;
+			}
+			return;
+		}
+
+		void print(Node<T>* node) {
+			if (!node) {
+				return;
+			}
+			print(node->_left);
+			cout << node->_value << " ";
+			print(node->_right);
+		}
+
+	public:
+
+		
---
+++ tests/tests.cpp
@@ -0,0 +1,157 @@
+#include "functions.cpp"
+#include <gtest/gtest.h>
+#include <iostream>
+
+
+
+using namespace tree;
+using namespace std;
+
+TEST(CreatedSet, Copying1) {
+	Node<int>* test1 = new Node<int>(1);
+	Node<int>* test2 = new Node<int>(4);
+	Node<int>* test_res = new Node<int>(2, test1, test2);
+	Set<int> a(test_res);
+	Set<int> b(1);
+	b = a;
+	b.print();
+}
+
+TEST(CreatedSet, CopyConstructor) {
+	Node<int>* test1 = new Node<int>(1);
+	Node<int>* test2 = new Node<int>(4);
+	Node<int>* test_res = new Node<int>(2, test1, test2);
+	Set<int> a(test_res);
+	Set<int> b(a);
+	b.print();
+}
+TEST(CreatedSet, InsertElement1) {
+	Node<int>* test1 = new Node<int>(1);
+	Node<int>* test2 = new Node<int>(4);
+	Node<int>* test_res = new Node<int>(2, test1, test2);
+	Set<int> a(test_res);
+	a.insert(5);
+	a.print();
+}
+TEST(CreatedSet, InsertElement2) {
+	Node<int>* test1 = new Node<int>(1);
+	Node<int>* test2 = new Node<int>(4);
+	Node<int>* test_res = new Node<int>(2, test1, test2);
+	Set<int> a(test_res);
+	a.insert(3);
+	a.print();
+}
+TEST(CreatedSet, InsertElement3) {
+	Node<int>* test1 = new Node<int>(1);
+	Node<int>* test2 = new Node<int>(4);
+	Node<int>* test_res = new Node<int>(2, test1, test2);
+	Set<int> a(test_res);
+	a.insert(0);
+	a.print();
+}
+TEST(CreatedSet, ContainElement) {
+	Node<int>* test1 = new Node<int>(1);
+	Node<int>* test2 = new Node<int>(4);
+	Node<int>* test_res = new Node<int>(2, test1, test2);
+	Set<int> a(test_res);
+	a.insert(0);
+	if (a.contain(0)) {
+		cout << "this set contain 0." << endl;
+	}
+	else cout << "this set is not contain 0." << endl;
+}
+TEST(CreatedSet, NotContainElement) {
+	Node<int>* test1 = new Node<int>(1);
+	Node<int>* test2 = new Node<int>(4);
+	Node<int>* test_res = new Node<int>(2, test1, test2);
+	Set<int> a(test_res);
+	a.insert(0);
+	if (a.contain(5)) {
+		cout << "this set contain 5." << endl;
+	}
+	else cout << "this set is not contain 5." << endl;
+}
+TEST(CreatedSet, EraseElement1) {
+	Set<int> a(1);
+	a.insert(0);
+	a.insert(5);
+	a.insert(3);
+	a.insert(2);
+	a.insert(4);
+	a.insert(6);
+	a.print();
+	a.erase(5);
+	a.print();
+}
+TEST(CreatedSet, EraseElement2) {
+	Set<int> a(1);
+	a.insert(0);
+	a.insert(5);
+	a.insert(3);
+	a.print();
+	a.erase(5);
+	a.print();
+	a.erase(3);
+	a.print();
+	a.erase(0);
+	a.print();
+}
+TEST(Logic_operations, EraseTime) {
+	Set<int> test_set_1(1);
+	test_set_1.insert(2);
+	test_set_1.insert(3);
+	test_set_1.insert(4);
+	Set<int> test_set_2(4);
+	test_set_2.insert(5);
+	test_set_2.insert(6);
+	test_set_2.insert(7);
+
+	Set<int> result_xor = logic_xor(test_set_1, test_set_2);
+	Set<int> result_or = logic_or(test_set_1, test_set_2);
+	result_xor.print();
+	result_or.print();
+}
+TEST(ComparisonOfVectorAndSet, FillTime) {
+	int number = 1000;
+	cout << number << " " << "Vector time: " << vector_fill_time(number, 100) << endl;
+	cout << number << " " << "Set time : " << set_fill_time(number, 100) << endl;
+	number = 10000;
+	cout << number << " " << "Vector time: " << vector_fill_time(number, 100) << endl;
+	cout << number << " " << "Set time : " << set_fill_time(number, 100) << endl;
+	number = 100000;
+	cout << number << " " << "Vector time: " << vector_fill_time(number, 100) << endl;
+	cout << number << " " << "Set time : " << set_fill_time(number, 100) << endl;
+}
+TEST(ComparisonOfVectorAndSet, ContainTime) {
+	int number = 1000;
+	cout << number << " " << "Vector time : " << vector_contain_element_time(number, 1000) << endl;
+	cout << number << " " << "Set time : " << set_contain_element_time(number, 1000) << endl;
+	number = 10000;
+	cout << number << " " << "Vector time : " << vector_contain_element_time(number, 1000) << endl;
+	cout << number << " " << "Set time : " << set_contain_element_time(number, 1000) << endl;
+	number = 100000;
+	cout << number << " " << "Vector time : " << vector_contain_element_time(number, 1000) << endl;
+	cout << number << " " << "Set time : " << set_contain_element_time(number, 1000) << endl;
+}
+TEST(ComparisonOfVectorAndSet, InsertTime) {
+	int number = 1000;
+	cout << number << " " << "Vector time: " << vector_insert_element_time(number, 1000) << endl;
+	cout << number << " " << "Set time: " << set_insert_element_time(number, 1000) << endl;
+	number = 10000;
+	cout << number << " " << "Vector time: " << vector_insert_element_time(number, 1000) << endl;
+	cout << number << " " << "Set time: " << set_insert_element_time(number, 1000) << endl;
+	number = 100000;
+	cout << number << " " << "Vector time: " << vector_insert_element_time(number, 1000) << endl;
+	cout << number << " " << "Set time: " << set_insert_element_time(number, 1000) << endl;
+}
+TEST(ComparisonOfVectorAndSet, EraseTime) {
+	int number = 1000;
+	cout << number << " " << "Vector time: " << vector_erase_element_time(number, 1000) << endl;
+	cout << number << " " << "Set time: " << set_erase_element_time(number, 1000) << endl;
+	number = 10000;
+	cout << number << " " << "Vector time: " << vector_erase_element_time(number, 1000) << endl;
+	cout << number << " " << "Set time: " << set_erase_element_time(number, 1000) << endl;
+	number = 100000;
+	cout << number << " " << "Vector time: " << vector_erase_element_time(number, 1000) << endl;
+	cout << number << " " << "Set time: " << set_erase_element_time(number, 1000) << endl;
+}
